945899e7e3cc92a87a0b39c70cb49ac6d8eaa01d
Abid Ansari <abid@gmail.com>
2025-08-26 23:25:38 +0530

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,13 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]'
+        }
       }
     ]
   }
